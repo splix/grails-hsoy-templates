275c79dcbf1e8b05654ff777bd2606e5e30f8d39
Igor Artamonov <igor@artamonov.ru>
2012-10-08 21:52:23 -0400

    initial version

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,44 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+
+        mavenLocal()
+        mavenCentral()
+        mavenRepo "http://maven.the6hours.com/release"
+        mavenRepo "https://raw.github.com/raymyers/raymyers-mvn-repo/master/releases"
+        mavenRepo "https://github.com/codedance/maven-repository/raw/master"
+
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.18'
+
+        compile 'com.the6hours:hsoy-templates:0.3-SNAPSHOT'
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:2.0.3",
+              ":rest-client-builder:1.0.2") {
+            export = false
+        }
+        runtime(":resources:1.2.RC2")
+    }
+}
---
+++ grails-app/resourceMappers/com/the6hours/hsoytemplates/HsoyTemplatesResourceMapper.groovy
@@ -0,0 +1,57 @@
+package com.the6hours.hsoytemplates
+
+import org.codehaus.groovy.grails.plugins.support.aware.GrailsApplicationAware
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.grails.plugin.resource.mapper.MapperPhase
+
+
+/**
+ * 
+ * @author Igor Artamonov (http://igorartamonov.com)
+ * @since 04.10.12
+ */
+class HsoyTemplatesResourceMapper implements GrailsApplicationAware {
+
+    GrailsApplication grailsApplication
+
+    def phase = MapperPhase.GENERATION
+
+    static defaultIncludes = ['**/*.hsoy']
+
+    def map(resource, config) {
+        File originalFile = resource.processedFile
+        File target
+
+        if (resource.sourceUrl) {
+            File input = getOriginalFileSystemFile(resource.sourceUrl);
+            target = new File(generateCompiledFileFromOriginal(originalFile.absolutePath))
+
+            if (log.debugEnabled) {
+                log.debug "Compiling hsoy file [${originalFile}] into [${target}]"
+            }
+
+            try {
+                HsoyJsCompiler compiler = new HsoyJsCompiler()
+
+                def w = target.newWriter()
+                w << compiler.compileToString(input.text, originalFile.name)
+                w.close()
+
+                resource.processedFile = target
+                resource.sourceUrlExtension = 'js'
+                resource.actualUrl = generateCompiledFileFromOriginal(resource.originalUrl)
+                resource.contentType = 'text/javascript'
+            } catch (e) {
+                log.error("Error compiling hsoy file: ${originalFile}", e)
+            }
+        }
+    }
+
+    private String generateCompiledFileFromOriginal(String original) {
+        original.replaceAll(/(?i)\.hsoy/, '.js')
+    }
+
+    private File getOriginalFileSystemFile(String sourcePath) {
+        grailsApplication.parentContext.getResource(sourcePath).file
+    }
+}
